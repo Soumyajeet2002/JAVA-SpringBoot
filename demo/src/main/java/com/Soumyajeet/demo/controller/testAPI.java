b39a03b3b49cf1dd3cf37b8613b17c87967481dc
Soumyajeet2002 <sinkus14@gmail.com>
2026-07-22 16:27:58 +0530

    Java Spring-Boot Initial

---
+++ demo/src/main/java/com/Soumyajeet/demo/controller/testAPI.java
@@ -0,0 +1,15 @@
+package com.Soumyajeet.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class testAPI {
+
+    @GetMapping("/test")
+    public String testAPI() {
+        return "This is a test API ofr my first JAVA sprign BOOT";
+    }
+}
